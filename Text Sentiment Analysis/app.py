4f1851742359746c612b6f0e869b6aed0e26e90d
Vardaan Bhatia <vardaanbhatia06@gmail.com>
2023-03-12 18:12:26 -0600

    preprocess.py, app.py added, repository rearranged

---
+++ Text Sentiment Analysis/app.py
@@ -0,0 +1,34 @@
+import speech_recognition 
+import flair
+from flair.models import TextClassifier
+from flair.data import Sentence
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import nltk
+nltk.download('vader_lexicon')
+
+sid = SentimentIntensityAnalyzer()
+
+
+def S2T_flair(audio_file):
+    rec = speech_recognition.Recognizer()
+    audio_file = audio_file
+
+    with speech_recognition.AudioFile(audio_file) as audio_data:
+        audio_data = rec.record(audio_data)
+        result = rec.recognize_google(audio_data, show_all=True)
+        transcript = result['alternative'][0]["transcript"]
+    classifier = TextClassifier.load('en-sentiment')
+    data = Sentence(transcript)
+    classifier.predict(data)
+    sentiment = data.labels[0].value
+    score = data.labels[0].score
+    polarity= sid.polarity_scores(transcript)
+    print(f"Voice to Text Data: {transcript}")
+    print(f"Sentiment: {sentiment}")
+    print(f"Flair Score: {score}")
+    print(f"Polarity Score: {polarity}")
+    return sentiment, score, transcript
+
+audio_file= r"AudioWAV\1001_TIE_DIS_XX.wav"
+
+S2T_flair(audio_file)
